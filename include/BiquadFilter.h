7ae6bd5d2abc7f19d735de46a5a5544459365cb8
Tue Dissing <tsd@lydbydissing.com>
2022-05-14 22:13:54 +0200

    small refactor

--- include/BiquadFilter.h
+++ include/BiquadFilter.h
@@ -72,17 +72,14 @@ public:
     // Generates coefficients for a high shelf biquad filter
     void generateHighShelfCoEffs(float f, float gain, float q)
     {
-        if (q <= 0.0001)
-        {
-            q = 0.0001;
-        }
+        q = safeMin(q);
         float Fs = 1;
 
         float A = sqrtf(pow(10, (double)gain / 20.0));
         float w0 = 2 * M_PI * f / Fs;
         float c = cosf(w0);
-        float s = sinf(w0);
-        float alpha = s / (2 * q);
+        //float s = sinf(w0);
+        float alpha = calcAlpha(w0, q);
 
         float b0 = A * ((A + 1) + (A - 1) * c + 2 * sqrtf(A) * alpha);
         float b1 = -2 * A * ((A - 1) + (A + 1) * c);
@@ -91,28 +88,28 @@ public:
         float a1 = 2 * ((A - 1) - (A + 1) * c);
         float a2 = (A + 1) - (A - 1) * c - 2 * sqrtf(A) * alpha;
 
+        /*
         std::lock_guard lock(processMutex);
         coeffs[0] = b0 / a0;
         coeffs[1] = b1 / a0;
         coeffs[2] = b2 / a0;
         coeffs[3] = a1 / a0;
         coeffs[4] = a2 / a0;
+        */
+        setCoefss(a0,a1, a2, b0, b1, b2);
     }
 
     // Generates coefficients for a low shelf biquad filter
     void generateLowShelfCoEffs(float f, float gain, float q)
     {
-        if (q <= 0.0001)
-        {
-            q = 0.0001;
-        }
+        q = safeMin(q);
         float Fs = 1;
 
         float A = sqrtf(pow(10, (double)gain / 20.0));
         float w0 = 2 * M_PI * f / Fs;
         float c = cosf(w0);
-        float s = sinf(w0);
-        float alpha = s / (2 * q);
+//        float s = sinf(w0);
+        float alpha = calcAlpha(w0, q);
 
         float b0 = A * ((A + 1) - (A - 1) * c + 2 * sqrtf(A) * alpha);
         float b1 = 2 * A * ((A - 1) - (A + 1) * c);
@@ -121,28 +118,27 @@ public:
         float a1 = -2 * ((A - 1) + (A + 1) * c);
         float a2 = (A + 1) + (A - 1) * c - 2 * sqrtf(A) * alpha;
 
-        std::lock_guard lock(processMutex);
+        /*std::lock_guard lock(processMutex);
         coeffs[0] = b0 / a0;
         coeffs[1] = b1 / a0;
         coeffs[2] = b2 / a0;
         coeffs[3] = a1 / a0;
         coeffs[4] = a2 / a0;
+        */
+        setCoefss(a0,a1, a2, b0, b1, b2);
     }
 
     // Generates coefficients for a notch biquad filter
     void generateNotchCoEffs(float f, float gain, float q)
     {
-        if (q <= 0.0001)
-        {
-            q = 0.0001;
-        }
+        q = safeMin(q);
         float Fs = 1;
 
         float A = sqrtf(pow(10, (double)gain / 20.0));
         float w0 = 2 * M_PI * f / Fs;
         float c = cosf(w0);
-        float s = sinf(w0);
-        float alpha = s / (2 * q);
+        //float s = sinf(w0);
+        float alpha = calcAlpha(w0, q);
 
         float b0 = 1 + alpha * A;
         float b1 = -2 * c;
@@ -151,12 +147,64 @@ public:
         float a1 = -2 * c;
         float a2 = 1 - alpha;
 
+/*
         std::scoped_lock lock(processMutex);
         coeffs[0] = b0 / a0;
         coeffs[1] = b1 / a0;
         coeffs[2] = b2 / a0;
         coeffs[3] = a1 / a0;
         coeffs[4] = a2 / a0;
+        */
+        setCoefss(a0,a1, a2, b0, b1, b2);
+
+    }
+
+    float calcAlpha(float w0, float q)
+    {
+      return sinf(w0) / (2 * q);
+    }
+
+    void generateLowPassCoeffs(float f, float gain, float q)
+    {
+      // Formula from here: https://webaudio.github.io/Audio-EQ-Cookbook/audio-eq-cookbook.html
+
+      q = safeMin(q);
+      float Fs = 1;
+
+      float w0 = 2 * M_PI * f / Fs;
+      float c = cosf(w0);
+      //float s = sinf(w0);
+      float alpha = calcAlpha(w0, q);
+
+      // coefficients
+      float b0 = (1 - c) / 2;
+      float b1 = 1 - c;
+      float b2 = b0;
+      float a0 = 1 + alpha;
+      float a1 = -2 * c;
+      float a2 = a0;
+
+      setCoefss(a0,a1, a2, b0, b1, b2);
+    }
+
+    float safeMin(int q)
+    {
+      if (q <= 0.0001)
+      {
+          return 0.0001;
+      }
+      return q;
+    }
+
+    // Is this a copy or a reference?
+    void setCoefss(float a0, float a1, float a2, float b0, float b1, float b2)
+    {
+      std::scoped_lock lock(processMutex);
+      coeffs[0] = b0 / a0;
+      coeffs[1] = b1 / a0;
+      coeffs[2] = b2 / a0;
+      coeffs[3] = a1 / a0;
+      coeffs[4] = a2 / a0;
     }
 
     // Generates coefficients for a peaking biquad filter
