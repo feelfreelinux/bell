49b8822d0fc27017a7789388cfd8c2cbb68a7782
Tue Dissing <tsd@lydbydissing.com>
2022-05-26 09:46:11 +0200

    small refactor to make the code mor generic

--- include/BiquadFilter.h
+++ include/BiquadFilter.h
@@ -14,18 +14,29 @@ private:
     float coeffs[5];
     float w[2] = {0, 0};
     float Fs = 1;
+    float sampleFrequency = 44100.0;
 
 public:
     BiquadFilter(){};
 
+    BiquadFilter(int sample_frequence)
+    {
+      sampleFrequency = sample_frequence;
+    }
+
     // Generates coefficients for a high pass biquad filter
     void generateHighPassCoEffs(float f, float q){
         q = safeMin(q);
+
+    //    EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew: f: %.2f, q: %.2f", f, q);
 //        float Fs = 1;
 
         float w0 = calcW0(f);
+  //      EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew: w0,: %.2f", w0);
         float c = cosf(w0);
+  //      EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew: c,: %.2f", c);
         float alpha = calcAlpha(w0,q);
+  //      EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew:alpha,: %.2f", alpha);
 
         float b0 = (1 + c) / 2;
         float b1 = -(1 + c);
@@ -34,7 +45,54 @@ public:
         float a1 = -2 * c;
         float a2 = 1 - alpha;
 
-        setCoefss(a0,a1, a2, b0, b1, b2);
+/*
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew: b0,: %.2f", b0);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew: b1,: %.2f", b1);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew: b2,: %.2f", b2);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew: a0,: %.2f", a0);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew: a1,: %.2f", a1);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffsNew: a2,: %.2f", a2);
+        */
+
+        setCoefss(a0, a1, a2, b0, b1, b2);
+    }
+
+    // Generates coefficients for a high pass biquad filter
+    void generateHighPassCoEffsOld(float f, float q){
+        if (q <= 0.0001) {
+            q = 0.0001;
+        }
+        float Fs = 1;
+
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: f: %.2f, q: %.2f", f, q);
+
+        float w0 = 2 * M_PI * (f/sampleFrequency) / Fs;
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: w0,: %.2f", w0);
+        float c = cosf(w0);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: c,: %.2f", c);
+        float s = sinf(w0);
+        float alpha = s / (2 * q);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: alpha,: %.2f", alpha);
+
+        float b0 = (1 + c) / 2;
+        float b1 = -(1 + c);
+        float b2 = b0;
+        float a0 = 1 + alpha;
+        float a1 = -2 * c;
+        float a2 = 1 - alpha;
+
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: b0,: %.2f", b0);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: b1,: %.2f", b1);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: b2,: %.2f", b2);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: a0,: %.2f", a0);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: a1,: %.2f", a1);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighPassCoEffs: a2,: %.2f", a2);
+
+        coeffs[0] = b0 / a0;
+        coeffs[1] = b1 / a0;
+        coeffs[2] = b2 / a0;
+        coeffs[3] = a1 / a0;
+        coeffs[4] = a2 / a0;
     }
 
     // Generates coefficients for a low pass biquad filter
@@ -54,11 +112,11 @@ public:
         float a1 = -2 * c;
         float a2 = 1 - alpha;
 
-        setCoefss(a0,a1, a2, b0, b1, b2);
+        setCoefss(a0, a1, a2, b0, b1, b2);
     }
 
     // Generates coefficients for a high shelf biquad filter
-    void generateHighShelfCoEffs(float f, float gain, float q)
+    void generateHighShelfCoEffsOld(float f, float gain, float q)
     {
         q = safeMin(q);
 //        float Fs = 1;
@@ -77,6 +135,42 @@ public:
 
         setCoefss(a0,a1, a2, b0, b1, b2);
     }
+
+    void generateHighShelfCoEffs(float f, float gain, float q)
+    {
+        // Formula from here: https://webaudio.github.io/Audio-EQ-Cookbook/audio-eq-cookbook.html
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: f: %.2f, gain: %.2f, q: %.6f", f, gain, q);
+        q = safeMin(q);
+//        float Fs = 1;
+
+        float A = calcA(gain);
+        float w0 = calcW0(f);
+        float c = cosf(w0);
+        float alpha = calcAlpha(w0,q);
+
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: f: %.2f, q: %.6f", f, q);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: w0,: %.2f", w0);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: c,: %.2f", c);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: alpha,: %.2f", alpha);
+
+        float b0 = A * ((A + 1) + ((A - 1) * c) + (2 * sqrtf(A) * alpha));
+        float b1 = -2 * A * ((A - 1) + ((A + 1) * c));
+        float b2 = A * ((A + 1) + ((A - 1) * c) - (2 * sqrtf(A) * alpha));
+        float a0 = (A + 1) - ((A - 1) * c) + (2 * sqrtf(A) * alpha);
+        float a1 = 2 * ((A - 1) - ((A + 1) * c));
+        float a2 = (A + 1) - ((A - 1) * c) - (2 * sqrtf(A) * alpha);
+
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: b0,: %.6f", b0);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: b1,: %.6f", b1);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: b2,: %.6f", b2);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: a0,: %.6f", a0);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: a1,: %.6f", a1);
+        EUPH_LOG(debug, "eq", "BiquadFilters; generateHighShelfCoEffs: a2,: %.6f", a2);
+
+        setCoefss(a0, a1, a2, b0, b1, b2);
+    }
+
+
 
     // Generates coefficients for a low shelf biquad filter
     void generateLowShelfCoEffs(float f, float gain, float q)
@@ -205,7 +299,7 @@ public:
         setCoefss(a0,a1, a2, b0, b1, b2);
     }
 
-    float safeMin(int q)
+    float safeMin(float q)
     {
       if (q <= 0.0001)
       {
@@ -221,7 +315,12 @@ public:
 
     float calcW0(float f)
     {
-      return 2 * M_PI * f / Fs;
+      return 2 * M_PI * (f / sampleFrequency);
+    }
+
+    float calcA(float gain)
+    {
+      return sqrtf(pow(10, (double)gain / 20.0));
     }
 
 
@@ -229,11 +328,24 @@ public:
     void setCoefss(float a0, float a1, float a2, float b0, float b1, float b2)
     {
       std::scoped_lock lock(processMutex);
-      coeffs[0] = b0 / a0;
-      coeffs[1] = b1 / a0;
-      coeffs[2] = b2 / a0;
-      coeffs[3] = a1 / a0;
-      coeffs[4] = a2 / a0;
+
+      float coeffs_b0 = b0 / a0;
+      float coeffs_b1 = b1 / a0;
+      float coeffs_b2 = b2 / a0;
+      float coeffs_a1 = a1 / a0;
+      float coeffs_a2 = a2 / a0;
+
+      EUPH_LOG(debug, "eq", "BiquadFilters; setCoefss: b0,: %.6f", coeffs_b0);
+      EUPH_LOG(debug, "eq", "BiquadFilters; setCoefss: b1,: %.6f", coeffs_b1);
+      EUPH_LOG(debug, "eq", "BiquadFilters; setCoefss: b2,: %.6f", coeffs_b2);
+      EUPH_LOG(debug, "eq", "BiquadFilters; setCoefss: a0,: %.6f", coeffs_a1);
+      EUPH_LOG(debug, "eq", "BiquadFilters; setCoefss: a1,: %.6f", coeffs_a2);
+
+      coeffs[0] = coeffs_b0;
+      coeffs[1] = coeffs_b1;
+      coeffs[2] = coeffs_b2;
+      coeffs[3] = coeffs_a1;
+      coeffs[4] = coeffs_a2;
     }
 
     void processSamples(float *input, int numSamples)
