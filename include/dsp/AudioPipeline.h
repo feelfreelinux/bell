ee80c547c725e2487a4f1be6a3d32387b7e05d99
Filip <f.grzywok@gmail.com>
2022-11-19 19:51:47 +0100

    fix: MBedTLS, AudioPipeline

--- include/dsp/AudioPipeline.h
+++ include/dsp/AudioPipeline.h
@@ -1,17 +1,18 @@
 #pragma once
 
-#include <memory>
-#include "StreamInfo.h"
 #include "AudioTransform.h"
+#include "StreamInfo.h"
+#include <memory>
 
-namespace bell::dsp
-{
-    class AudioPipeline
-    {
-    private:
-        std::vector<std::unique_ptr<AudioTransform>> transforms;
-    public:
-        void addTransform(std::unique_ptr<AudioTransform> transform);
-        std::unique_ptr<StreamInfo> process(std::unique_ptr<StreamInfo> data);
-    };
+namespace bell::dsp {
+class AudioPipeline {
+  private:
+    std::vector<std::unique_ptr<AudioTransform>> transforms;
+
+  public:
+    AudioPipeline();
+    ~AudioPipeline(){};
+    void addTransform(std::unique_ptr<AudioTransform> transform);
+    std::unique_ptr<StreamInfo> process(std::unique_ptr<StreamInfo> data);
 };
+}; // namespace bell::dsp
