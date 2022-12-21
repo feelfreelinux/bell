5423d230e2196ee99ec44007d6c07c0fe55f28cc
Filip Grzywok <f.grzywok@gmail.com>
2022-12-21 14:58:13 +0100

    feat: CentralAudioBuffer rework

--- main/audio-dsp/include/CentralAudioBuffer.h
+++ main/audio-dsp/include/CentralAudioBuffer.h
@@ -4,104 +4,142 @@
 #include <cmath>
 #include <memory>
 
+#include "BellUtils.h"
 #include "CircularBuffer.h"
 #include "StreamInfo.h"
-#include "BellUtils.h"
 
 typedef std::function<void(std::string)> shutdownEventHandler;
 
 namespace bell {
 class CentralAudioBuffer {
-  private:
-	std::mutex accessMutex;
+ private:
+  std::mutex accessMutex;
 
-	std::atomic<bool> isLocked = false;
+  std::atomic<bool> isLocked = false;
 
-  public:
-	CentralAudioBuffer(size_t size) {
-		audioBuffer = std::make_shared<CircularBuffer>(size);
-	}
-	
-	std::shared_ptr<bell::CircularBuffer> audioBuffer;
-	uint32_t sampleRate		   = 44100;
+ public:
+  static const size_t PCM_CHUNK_SIZE = 4096;
 
-	/**
-	 * Sends an event which reconfigures current audio output
-	 * @param format incoming sample format
-	 * @param sampleRate data's sample rate
-	 */
-	void configureOutput(bell::BitWidth format, uint32_t sampleRate) {
-		if (this->sampleRate != sampleRate) {
-			this->sampleRate = sampleRate;
-		}
-	}
+  // Audio marker for track change detection, and DSP autoconfig
+  struct AudioChunk {
+    // Unique track hash, used for track change detection
+    size_t trackHash;
 
-	/**
+    // Audio format
+    uint32_t sampleRate;
+    uint8_t channels;
+    uint8_t bitDepth;
+
+    // PCM data size
+    size_t pcmSize;
+
+    // PCM data
+    uint8_t pcmData[PCM_CHUNK_SIZE];
+  };
+
+  CentralAudioBuffer(size_t chunks) {
+    audioBuffer = std::make_shared<CircularBuffer>(chunks * sizeof(AudioChunk));
+  }
+
+  std::shared_ptr<bell::CircularBuffer> audioBuffer;
+  uint32_t currentSampleRate = 44100;
+
+  /**
 	 * Returns current sample rate
 	 * @return sample rate
 	 */
-	uint32_t getSampleRate() {
-		return sampleRate;
-	}
+  uint32_t getSampleRate() { return currentSampleRate; }
 
-	/**
+  /**
 	 * Clears input buffer, to be called for track change and such
 	 */
-	void clearBuffer() {
-		audioBuffer->emptyExcept(this->sampleRate);
-	}
+  void clearBuffer() {
+    // size_t exceptSize = currentSampleRate + (sizeof(AudioChunk) - (currentSampleRate % sizeof(AudioChunk)));
+    // audioBuffer->emptyExcept(exceptSize);
+    audioBuffer->emptyBuffer();
+  }
 
-	/**
+  /**
 	 * Locks access to audio buffer. Call after starting playback
 	 */
-	void lockAccess() {
-		if (!isLocked) {
-			clearBuffer();
-			this->accessMutex.lock();
-			isLocked = true;
-		}
-	}
+  void lockAccess() {
+    if (!isLocked) {
+      clearBuffer();
+      this->accessMutex.lock();
+      isLocked = true;
+    }
+  }
 
-	/**
+  /**
 	 * Frees access to the audio buffer. Call during shutdown
 	 */
-	void unlockAccess() {
-		if (isLocked) {
-			clearBuffer();
-			this->accessMutex.unlock();
-			isLocked = false;
-		}
-	}
+  void unlockAccess() {
+    if (isLocked) {
+      clearBuffer();
+      this->accessMutex.unlock();
+      isLocked = false;
+    }
+  }
 
-	size_t read(uint8_t* dst, size_t bytes) {
-		size_t readBytes = this->audioBuffer->read(dst, bytes);
-		return readBytes;
-	}
+  bool hasAtLeast(size_t chunks) {
+    return audioBuffer->size() >= sizeof(AudioChunk) * chunks;
+  }
 
-	/**
-	 * Write audio data to the main buffer
-	 * @param data pointer to raw PCM data
-	 * @param bytes number of bytes to be read from provided pointer
-	 * @return number of bytes read
-	 */
-	size_t write(const uint8_t *data, size_t bytes) {
-		size_t bytesWritten = 0;
-		while (bytesWritten < bytes) {
-			auto write = audioBuffer->write(data + bytesWritten, bytes - bytesWritten);
-			bytesWritten += write;
+  AudioChunk currentChunk = {.pcmSize = 0};
+  bool hasChunk = false;
 
-			if (write == 0) {
-                //audioBuffer->dataSemaphore->wait();
-				BELL_SLEEP_MS(10);
-			}
-		}
+  AudioChunk lastReadChunk = {.pcmSize = 0};
 
-		return bytesWritten;
-	}
+  AudioChunk readChunk() {
+    if (audioBuffer->size() < sizeof(AudioChunk)) {
+      lastReadChunk.pcmSize = 0;
+      return lastReadChunk;
+    }
 
-	bool hasAtLeast(size_t bytes) {
-		return audioBuffer->size() >= bytes;
-	}
+    auto readBytes =
+        audioBuffer->read((uint8_t*)&lastReadChunk, sizeof(AudioChunk));
+    return lastReadChunk;
+  }
+
+  size_t writePCM(const uint8_t* data, size_t dataSize, size_t hash,
+                  uint32_t sampleRate = 44100, uint8_t channels = 2,
+                  uint8_t bitDepth = 16) {
+    if (hasChunk && (currentChunk.trackHash != hash ||
+                     currentChunk.pcmSize >= PCM_CHUNK_SIZE)) {
+
+      if ((audioBuffer->size() - audioBuffer->capacity()) <
+          sizeof(AudioChunk)) {
+        return 0;
+      }
+
+      // Track changed or buf full, return current chunk
+      hasChunk = false;
+      this->audioBuffer->write((uint8_t*)&currentChunk, sizeof(AudioChunk));
+    }
+
+		// New chunk requested, initialize
+    if (!hasChunk) {
+      currentChunk.trackHash = hash;
+      currentChunk.sampleRate = sampleRate;
+      currentChunk.channels = channels;
+      currentChunk.bitDepth = bitDepth;
+      currentChunk.pcmSize = 0;
+      hasChunk = true;
+    }
+
+		// Calculate how much data we can write
+    size_t toWriteSize = dataSize;
+
+    if (currentChunk.pcmSize + toWriteSize > PCM_CHUNK_SIZE) {
+      toWriteSize = PCM_CHUNK_SIZE - currentChunk.pcmSize;
+    }
+
+		// Copy it over :) 
+    memcpy(currentChunk.pcmData + currentChunk.pcmSize, data, toWriteSize);
+    currentChunk.pcmSize += toWriteSize;
+
+    return toWriteSize;
+  }
 };
 
-} // namespace bell
+}  // namespace bell
