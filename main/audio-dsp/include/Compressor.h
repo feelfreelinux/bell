27227693d2207bbfc384b2c1e41289dedb64f40c
Filip Grzywok <f.grzywok@gmail.com>
2022-12-27 12:16:14 +0100

    fix: build issue

--- main/audio-dsp/include/Compressor.h
+++ main/audio-dsp/include/Compressor.h
@@ -16,20 +16,7 @@
 
 // This is a fast approximation to log2()
 // Y = C[0]*F*F*F + C[1]*F*F + C[2]*F + C[3] + E;
-float log2f_approx(float X) {
-  float Y, F;
-  int E;
-  F = frexpf(fabsf(X), &E);
-  Y = 1.23149591368684f;
-  Y *= F;
-  Y += -4.11852516267426f;
-  Y *= F;
-  Y += 6.02197014179219f;
-  Y *= F;
-  Y += -3.13396450166353f;
-  Y += E;
-  return(Y);
-}
+float log2f_approx(float X); 
 
 #define log10f_fast(x)  (log2f_approx(x)*0.3010299956639812f)
 
